5f1c0e3a67d4802423e86028c1f74a88533347bd
alisgonzalez95 <alisonpozo95@gmail.com>
2018-01-29 16:53:37 -0500

    map

--- js/map.js
+++ js/map.js
@@ -1,8 +1,17 @@
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(-2.1587294, -79.8895231),
-    zoom: 17
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
-}
+if ($('#googleMap').length) {
+      var initialize = function () {
+          var mapOptions = {
+              zoom: 15,
+              scrollwheel: false,
+              center: new google.maps.LatLng(-2.158131,-79.887279)
+          };
+          var map = new google.maps.Map(document.getElementById("googleMap"),
+              mapOptions);
+          var marker = new google.maps.Marker({
+              position: map.getCenter(),
+              animation: google.maps.Animation.BOUNCE,
+              icon: 'img/map-marker.png',
+              map: map
+          });
+      }
+      google.maps.event.addDomListener(window, "load", initialize);
